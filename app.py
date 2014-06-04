d90a96d1e9f8cbd9cf781c0bd1d47ba8bb70c78a
Rand Seay <rand.seay@gmail.com>
2014-06-03 21:36:01 -0500

    Begin adding in the registration page.

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from flask import Flask, render_template
 
 app = Flask('GGFRC')
+app.config['DEBUG'] = True
 
 @app.route("/")
 def home():
     return render_template("home.html")
+
+@app.route("/fll/registration")
+def fll_registration():
+    return render_template("fll/registration.html")
 
 if __name__ == '__main__':
     app.run()
