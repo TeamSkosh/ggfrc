3e511210b18057a6c4c26c2dc19268b3ae841d01
Nicholas Zaccardi <nick@skoshinteractive.com>
2014-08-07 21:17:22 -0500

    Pulled in the sign-up

--- ggfrc/views.py
+++ ggfrc/views.py
@@ -1,5 +1,11 @@
-from ggfrc import app, render_template
+from ggfrc import app
+from flask import render_template
+
 
 @app.route("/")
 def home():
     return render_template("home.html")
+
+@app.route("/fll/registration", methods=('GET', 'POST'))
+def fll_registration():
+    return render_template("fll/registration.html")
