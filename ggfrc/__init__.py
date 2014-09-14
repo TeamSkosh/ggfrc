b7217537f949f329db6b1bc6567c2a230f1de73a
Nicholas Zaccardi <nick@skoshinteractive.com>
2014-09-14 15:08:51 -0500

    Added configuration of the flask system. Closes #26

--- ggfrc/__init__.py
+++ ggfrc/__init__.py
@@ -1,5 +1,10 @@
 from flask import Flask, render_template
+from flask_ini import FlaskIni
 
 app = Flask('ggfrc')
+
+with app.app_context():
+    app.iniconfig = FlaskIni()
+    app.iniconfig.read('local.config')
 
 from ggfrc import views
