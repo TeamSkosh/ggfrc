16a58a4b75444d44d4d1a0b44742f9fc4c563a18
Nicholas Zaccardi <nick@skoshinteractive.com>
2014-09-20 21:34:23 -0500

    Added the auto create user logic

--- ggfrc/blueprints/auth.py
+++ ggfrc/blueprints/auth.py
@@ -1,5 +1,5 @@
 from flask import current_app, Blueprint, session, g, request
-from ggfrc import models
+from ggfrc import models, db
 import requests
 
 bp = Blueprint('persona', __name__, url_prefix="/_auth")
@@ -8,25 +8,30 @@ bp = Blueprint('persona', __name__, url_prefix="/_auth")
 def get_current_user():
     g.user = None
     email = session.get('email')
+
     if email is not None:
+        user = models.User.get_user_by_email(email)
         g.user = email
 
 @bp.route('/login', methods=['GET', 'POST'])
 def login():
 
-    resp = requests.post(current_app.iniconfig.get('persona', 'PERSONA_VERIFIER'), data={
-        'assertion': request.form['assertion'],
-        'audience': request.host_url,
-    }, verify=True)
+    resp = _verify_user(request.form['assertion'], request.host_url)
 
-    if resp.ok:
+    if not resp or resp.ok:
         verification_data = resp.json()
 
         if verification_data['status'] == 'okay':
             email = verification_data['email']
 
-            if not models.User.get_user(email):
-                pass
+            # This user has never logged in before
+            if not models.User.get_user_by_email(email):
+                user = models.User(email=email)
+                db.session.add(user)
+                saved = db.session.commit()
+
+                if not saved:
+                    return "USER NOT CREATED", 500
             else:
                 session['email'] = email
 
@@ -34,6 +39,16 @@ def login():
 
     abort(400)
 
+
+def _verify_user(assertion, audience):
+
+    if not assertion and not audience:
+        raise RuntimeError("Cannot assert nothing")
+
+    return requests.post(current_app.iniconfig.get('persona', 'PERSONA_VERIFIER'), data={
+        'assertion': assertion,
+        'audience': audience
+    }, verify=True)
 
 @bp.route('/logout', methods=['POST'])
 def logout():
