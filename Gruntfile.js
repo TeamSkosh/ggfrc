b45f7c6ff8ba8687b91635a17638d72b035f8544
Rand Seay <rand.seay@gmail.com>
2015-10-19 21:30:51 -0500

    Initialize jekyll version of the site.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,145 +1,188 @@
 module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
 
-  "use strict";
-
-  grunt.initConfig({
-    pkg: grunt.file.readJSON("package.json"),
-
-    uglify: {
-      global: {
-        files: {
-          "static/js/global.min.js": ["static/js/global.js"]
-        }
-      }
-    },
-
-    sass: {
-      options: {
-        includePaths: ["bower_components/foundation/scss"]
-      },
-      global: {
-        options: {
-          outputStyle: "compressed"
+        concurrent: {
+            serve: [
+                'sass',
+                'watch',
+                'shell:jekyllServe'
+            ],
+            options: {
+                logConcurrentOutput: true
+            }
         },
-        files: {
-          "static/css/global.min.css": "static/scss/global.scss"
-        }
-      },
-      foundation: {
-        options: {
-          outputStyle: "compressed"
+
+        copy: {
+            fontawesome: {
+                files: [
+                    {
+                        expand: true,
+                        src: [
+                            "bower_components/fontawesome/css/font-awesome.min.css"
+                        ],
+                        dest: "css/",
+                        flatten: true,
+                        filter: "isFile"
+                    },
+                    {
+                        expand: true,
+                        src: [
+                            "bower_components/fontawesome/fonts/fontawesome-webfont.*"
+                        ],
+                        dest: "fonts/",
+                        flatten: true,
+                        filter: "isFile"
+                    }
+                ]
+            },
+            fuselage: {
+                files: [
+                    {
+                        expand: true,
+                        src: [
+                            "bower_components/fuselage/scss/fuselage.scss",
+                            "bower_components/fuselage/scss/_settings.scss"
+                        ],
+                        dest: "_sass/",
+                        flatten: true,
+                        filter: "isFile"
+                    }
+                ]
+            },
+            styles: {
+                files: [
+                    {
+                        expand: true,
+                        src: [
+                            "_site/css/main.min.css"
+                        ],
+                        dest: "css/",
+                        flatten: true,
+                        filter: "isFile"
+                    },
+                    {
+                        expand: true,
+                        src: [
+                            "_site/css/examples/*.css"
+                        ],
+                        dest: "css/examples/",
+                        flatten: true,
+                        filter: "isFile"
+                    }
+                ]
+            }
         },
-        files: {
-          "static/css/foundation.min.custom.css": "static/scss/foundation-custom.scss"
-        }
-      }
-    },
 
-    autoprefixer: {
-      global: {
-        src: "static/css/global.min.css"
-      }
-    },
-
-    watch: {
-      options: {
-        livereload: true
-      },
-      global_styles: {
-        files: ["static/scss/global.scss"],
-        tasks: ["sass:global", "autoprefixer"]
-      },
-      foundation_styles: {
-        files: ["static/scss/_foundation-settings.scss", "static/foundation-custom.scss"],
-        tasks: ["sass:foundation"]
-      },
-      js: {
-        files: ["static/js/*.js"],
-        tasks: ["uglify"]
-      },
-      svg: {
-        files: ["static/svg/*.svg"],
-        tasks: ["newer:svgstore"]
-      },
-      img: {
-        files: ["static/img/*.png"],
-        tasks: ["newer:imagemin:png"]
-      }
-    },
-
-    svgstore: {
-      options: {
-        prefix : "shape-",
-        cleanup: false,
-        symbol: {
-          width: "100%"
+        imagemin: {
+            dynamic: {
+                files : [{
+                    expand: true,
+                    cwd: 'img/',
+                    src: '**/*.{gif,jpeg,jpg,png}',
+                    dest: 'img/'
+                }]
+            }
         },
-        svg: {
-          style: "display: none;"
+
+        sass: {
+            options: {
+                includePaths: ['bower_components/fuselage/scss/components'],
+                outputStyle: 'compressed',
+                sourceMap: true
+            },
+            dist: {
+                files: [
+                    {
+                        '_site/css/main.min.css': '_sass/main.scss'
+                    },
+                    {
+                        expand: true,
+                        cwd: '_sass',
+                        src: ['examples/*.scss'],
+                        dest: '_site/css',
+                        ext: '.css'
+                    }
+                ]
+            }
+        },
+
+        shell: {
+            jekyllServe: {
+                command: "jekyll serve --baseurl="
+            },
+            jekyllBuild: {
+                command: "jekyll build --config _config-dev.yml"
+            },
+            jekyllPrepDeploy: {
+                command: "jekyll build --config _config.yml"
+            }
+        },
+
+        svgstore: {
+            options: {
+                prefix : "shape-",
+                cleanup: false,
+                symbol: {
+                    width: "100%"
+                },
+                svg: {
+                    style: "display: none;"
+                }
+            },
+            default: {
+                files: {
+                    "img/svg/svg-defs.svg": ["img/svg/shapes/*.svg"]
+                }
+            }
+        },
+
+        watch: {
+            images: {
+                files: ['img/**/*.{gif,jpeg,jpg,png}'],
+                tasks: ['newer:imagemin']
+            },
+            styles: {
+                files: ['_sass/**/*.scss', '_sass/*.scss'],
+                tasks: ['sass']
+            },
+            svg: {
+              files: ['img/svg/*.svg'],
+              tasks: ['newer:svgstore']
+            }
         }
-      },
-      default: {
-        files: {
-          "static/svg/svg-defs.svg": ["static/svg/shapes/*.svg"]
-        }
-      }
-    },
+    });
 
-    imagemin: {
-      png: {
-        files: [{
-          expand: true,
-          cwd: "static/img/",
-          src: ["*.png"],
-          dest: "static/img/",
-          ext: ".min.png"
-        }]
-      }
-    },
+    require("load-grunt-tasks")(grunt);
 
-    copy: {
-      foundation_js: {
-        files: [{
-          expand: true,
-          src: ["bower_components/foundation/js/foundation.min.js", "bower_components/foundation/js/vendor/fastclick.js"],
-          dest: "static/js/",
-          flatten: true,
-          filter: "isFile"
-        }]
-      }
-    }
+    grunt.registerTask('serve', [
+        'newer:copy:fontawesome',
+        'newer:copy:fuselage',
+        'newer:copy:styles',
+        'newer:imagemin',
+        'sass',
+        'concurrent:serve'
+    ]);
 
-  });
+    grunt.registerTask('build', [
+        'newer:copy:fontawesome',
+        'newer:svgstore',
+        'newer:imagemin',
+        'newer:copy:fuselage',
+        'newer:copy:styles',
+        'shell:jekyllBuild',
+        'sass',
+    ]);
 
-  require("load-grunt-tasks")(grunt);
+    grunt.registerTask('prep-deploy', [
+        'newer:copy:fontawesome',
+        'newer:svgstore',
+        'newer:imagemin',
+        'newer:copy:fuselage',
+        'shell:jekyllPrepDeploy',
+        'sass',
+        'newer:copy:styles'
+    ]);
 
-  grunt.registerTask("styles", ["newer:sass:global", "newer:sass:foundation", "newer:autoprefixer"]);
-
-  grunt.registerTask("js", ["newer:uglify", "newer:copy:foundation_js"]);
-
-  grunt.registerTask("images", [
-    "newer:svgstore",
-    "newer:imagemin:png"
-  ]);
-
-  grunt.registerTask("build", [
-    "sass:global",
-    "sass:foundation",
-    "newer:autoprefixer",
-    "uglify",
-    "newer:svgstore",
-    "newer:imagemin:png",
-    "newer:copy:foundation_js"
-  ]);
-
-  grunt.registerTask("default", [
-    "sass:global",
-    "sass:foundation",
-    "autoprefixer",
-    "uglify",
-    "newer:svgstore",
-    "newer:imagemin:png",
-    "watch"
-  ]);
-
-};
+    grunt.registerTask('default', ['build']);
+}
